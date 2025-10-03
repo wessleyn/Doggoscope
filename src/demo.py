15e84acf0fc1e0783072dbdc9374eb7ef88fee91
wessleyn <wessleynyakaz@gmail.com>
2025-10-03 10:20:15 +0200

    added readImage function

--- src/demo.py
+++ src/demo.py
@@ -1,5 +1,17 @@
+import os
+
 import cv2 as cv
+import matplotlib.pyplot as plt  # type: ignore
+import numpy as np
+
+
+def readImage():
+    root = os.getcwd()
+    imgPath = os.path.join(root, "../assets/demoImages", "cutePic1.jpeg")
+    img = cv.imread(imgPath)
+    cv.imshow("Cute Pic", img)
+    cv.waitKey(0)
 
 if __name__ == "__main__":
     print(cv.__version__)
-    
+    readImage()
