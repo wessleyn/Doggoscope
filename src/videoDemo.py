7e974a7cc65c9b4664fba0111b4193315f7a38cf
wessleyn <wessleynyakaz@gmail.com>
2025-10-03 12:10:07 +0200

    refactor videoDemo.py to improve file path handling and enhance writeVideoToFile functionality

--- src/videoDemo.py
+++ src/videoDemo.py
@@ -6,7 +6,9 @@ import numpy as np
 
 CAMERA_INDEX = 0
 ROOT = os.path.abspath("../assets/")
-
+FRPS = 20
+HEIGHT = 480
+WIDTH = 640
 
 def videoFromCam():
     cap = cv.VideoCapture(CAMERA_INDEX)
@@ -30,8 +32,7 @@ def videoFromCam():
 
 
 def videoFromFile():
-    root = os.getcwd()
-    videoPath = os.path.join(root, "assets", "demoVideos", "mirrorChasing.mp4")
+    videoPath = os.path.join(ROOT, "demoVideos", "mirrorChasing.mp4")
     cap = cv.VideoCapture(videoPath)
 
     while cap.isOpened():
@@ -45,7 +46,29 @@ def videoFromFile():
         if cv.waitKey(16) == ord("q"):
             break
 
+def writeVideoToFile():
+    cap = cv.VideoCapture(CAMERA_INDEX)
 
+    fourcc = cv.VideoWriter_fourcc(*"mp4v")
+    outPath = os.path.join(ROOT, "output", "webcam.mp4")
+    out = cv.VideoWriter(outPath, fourcc, FRPS, (WIDTH, HEIGHT))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        else:
+            out.write(frame)
+            cv.imshow("frame", frame)
+
+        if cv.waitKey(16) == ord("q"):
+            break
+
+    cap.release()
+    out.release()
+    cv.destroyAllWindows()
 if __name__ == "__main__":
     # videoFromCam()
-    videoFromFile()
+    # videoFromFile()
+    writeVideoToFile()
