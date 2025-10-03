1f422f5289ee91d70a230c77da14cff2ab0dfe6f
wessleyn <wessleynyakaz@gmail.com>
2025-10-03 11:00:40 +0200

    added writeImage function

--- src/demo.py
+++ src/demo.py
@@ -4,14 +4,23 @@ import cv2 as cv
 import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
 
+ROOT = os.path.abspath("../assets/")
 
 def readImage():
-    root = os.getcwd()
-    imgPath = os.path.join(root, "../assets/demoImages", "cutePic1.jpeg")
+    imgPath = os.path.join(ROOT, "demoImages", "cutePic1.jpeg")
     img = cv.imread(imgPath)
-    cv.imshow("Cute Pic", img)
+    debug = 1
+    cv.imshow("img", img)
     cv.waitKey(0)
+
+def writeImage():
+    imgPath = os.path.join(ROOT, "demoImages", "cutePic1.jpeg")
+    img = cv.imread(imgPath)
+    os.makedirs(os.path.join(ROOT, "output"), exist_ok=True)
+    writePath = os.path.join(ROOT, "output", "output.png")
+    cv.imwrite(writePath, img)
 
 if __name__ == "__main__":
     print(cv.__version__)
-    readImage()
+    # readImage()
+    writeImage()
